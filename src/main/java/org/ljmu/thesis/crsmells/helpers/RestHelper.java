b01a73c85b25627fbb02a6bf3197c3177cb2175f
Sahil Srivastava <sahil.srivastava@powerschool.com>
2022-10-12 23:23:08 +0530

    some progress

---
+++ src/main/java/org/ljmu/thesis/crsmells/helpers/RestHelper.java
@@ -0,0 +1,21 @@
+package org.ljmu.thesis.crsmells;
+
+import org.apache.http.client.methods.CloseableHttpResponse;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.HttpClients;
+import org.apache.http.util.EntityUtils;
+
+import java.io.IOException;
+
+public class RestHelper {
+    public static String get(String uri) throws IOException {
+        HttpGet request = new HttpGet(uri);
+        try (CloseableHttpResponse response = HttpClients.createDefault().execute(request)) {
+            String stringOutput = EntityUtils.toString(response.getEntity());
+            if (stringOutput.startsWith(")")) {
+                return stringOutput.substring(4);
+            }
+            return stringOutput;
+        }
+    }
+}
