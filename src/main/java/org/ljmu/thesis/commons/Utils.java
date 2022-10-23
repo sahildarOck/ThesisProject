6c3f65d0e60168ced91c5b06afcca070c9607f12
Sahil Srivastava <sahil.srivastava@powerschool.com>
2022-10-23 09:24:41 +0530

    fix: pmd run issue in Win

--- src/main/java/org/ljmu/thesis/commons/Utils.java
+++ src/main/java/org/ljmu/thesis/commons/Utils.java
@@ -4,4 +4,8 @@ public class Utils {
     public static String getPRUrlFromRevisionUrl(String revisionUrl) {
         return revisionUrl.substring(0, revisionUrl.lastIndexOf("/"));
     }
+
+    public static boolean isMacOS() {
+        return System.getProperty("os.name").trim().contains("Mac");
+    }
 }
--- src/main/java/org/ljmu/thesis/helpers/codesmells/PmdHelper.java
+++ src/main/java/org/ljmu/thesis/helpers/codesmells/PmdHelper.java
@@ -1,5 +1,6 @@
 package org.ljmu.thesis.helpers.codesmells;
 
+import org.ljmu.thesis.commons.Utils;
 import org.ljmu.thesis.helpers.ConfigHelper;
 
 import java.io.File;
@@ -8,12 +9,16 @@ import java.util.Arrays;
 import java.util.List;
 
 public class PmdHelper {
+    private static String PMD_RUN_MAC_FILE = "run.sh";
+    private static String PMD_RUN_WIN_FILE = "pmd.bat";
+
     public static String startPmdCodeSmellProcessAndGetOutput(String projectPath, String filePath) throws IOException {
         return ProcessBuilderHelper.startProcessAndGetOutput(ConfigHelper.getPmdBinPath(), getPmdCommands(projectPath, filePath));
     }
 
     private static List<String> getPmdCommands(String projectPath, String filePath) throws IOException {
-        return Arrays.asList("./run.sh", "pmd", "--no-cache", "-d", projectPath + File.separator + filePath,
+        String pmdFileToRun = Utils.isMacOS() ? PMD_RUN_MAC_FILE : PMD_RUN_WIN_FILE;
+        return Arrays.asList("." + File.separator + pmdFileToRun, "pmd", "--no-cache", "-d", projectPath + File.separator + filePath,
                 "-f", "json", "-R", ConfigHelper.getCodeSmellRulesFilePath());
     }
 }
